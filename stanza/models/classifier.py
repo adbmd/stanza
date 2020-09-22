9c69b1cba0fa53e6d72e0a2790291c996bebf190
John Bauer <horatio@gmail.com>
2020-09-22 13:35:48 -0700

    First pass at adding elmo to sentiment
    Switch from allennlp elmo to elmoformanylangs

--- stanza/models/classifier.py
+++ stanza/models/classifier.py
@@ -30,6 +30,7 @@ class DevScoring(Enum):
     WEIGHTED_F1 = 'WF'
 
 logger = logging.getLogger('stanza')
+logging.getLogger('elmoformanylangs').setLevel(logging.WARNING)
 
 DEFAULT_TRAIN='extern_data/sentiment/sst-processed/fiveclass/train-phrases.txt'
 DEFAULT_DEV='extern_data/sentiment/sst-processed/fiveclass/dev-roots.txt'
@@ -154,6 +155,11 @@ def parse_args():
                         help="Whether to use regular cross entropy or scale it by 1/log(quantity)")
     parser.add_argument('--min_train_len', type=int, default=0,
                         help="Filter sentences less than this length")
+
+    parser.add_argument('--elmo_model', default='extern_data/manyelmo/english',
+                        help='Directory with elmo model')
+    parser.add_argument('--use_elmo', dest='use_elmo', default=False, action='store_true',
+                        help='Use an elmo model as a source of parameters')
 
     args = parser.parse_args()
     return args
@@ -532,6 +538,15 @@ def load_pretrain(args):
     return pretrain
 
 
+def load_elmo(elmo_model):
+    # This import is here so that Elmo integration can be treated
+    # as an optional feature
+    import elmoformanylangs
+
+    logger.info("Loading elmo: %s" % elmo_model)
+    elmo_model = elmoformanylangs.Embedder(elmo_model)
+    return elmo_model
+
 def print_args(args):
     """
     For record keeping purposes, print out the arguments when training
@@ -561,14 +576,15 @@ def main():
         train_set = None
 
     pretrain = load_pretrain(args)
+    elmo_model = load_elmo(args.elmo_model) if args.use_elmo else None
 
     if args.load_name:
-        model = cnn_classifier.load(args.load_name, pretrain)
+        model = cnn_classifier.load(args.load_name, pretrain, elmo_model)
     else:
         assert train_set is not None
         labels = dataset_labels(train_set)
         extra_vocab = dataset_vocab(train_set)
-        model = cnn_classifier.CNNClassifier(pretrain, extra_vocab, labels, args)
+        model = cnn_classifier.CNNClassifier(pretrain, extra_vocab, elmo_model, labels, args)
 
     if args.cuda:
         model.cuda()
